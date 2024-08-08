ef8f49b61678ec3c61ec524fbcf206bbd2ce92ce
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2024-08-08 14:53:51 +0100

    Remove unneeded code from Legacy example

--- samples/Legacy/LegacyWebApplication/Global.asax.cs
+++ samples/Legacy/LegacyWebApplication/Global.asax.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -8,7 +6,6 @@ namespace LegacyWebApplication
 {
     public class MvcApplication : C3D.Extensions.SystemWeb.OpenTelemetry.Application.OpenTelemeteryApplication
     {
-        private IServiceProvider serviceProvider;
         protected override void Application_Start()
         {
             base.Application_Start();
@@ -17,12 +14,6 @@ namespace LegacyWebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-        }
-
-        protected override void UseServiceProvider(ServiceProvider serviceProvider)
-        {
-            this.serviceProvider = serviceProvider;
-            base.UseServiceProvider(serviceProvider);
         }
     }
 }
