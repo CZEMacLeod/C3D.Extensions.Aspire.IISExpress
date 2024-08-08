9efce461bf5528e9bc5f2c7719b3c5c085f3138e
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2024-08-08 14:19:12 +0100

    Update all packages and change Legacy example to use http to work around SSL issue.

--- samples/Legacy/AspireAppHost3/Program.cs
+++ samples/Legacy/AspireAppHost3/Program.cs
@@ -1,8 +1,14 @@
-var builder = DistributedApplication.CreateBuilder(args);
+var builder = DistributedApplication.CreateBuilder(new DistributedApplicationOptions()
+{
+    Args = args,
+    // NET Framework has an issue pushing OTLP data using GRPC and SSL - use http instead of https
+    AllowUnsecuredTransport = true
+});
 
 var legacy = builder.AddProject<Projects.LegacyWebApplication>("legacy")
     // ports are not automatically picked up - need to manually configure
     .WithHttpsEndpoint(44372, isProxied: false)
-    .WithHttpEndpoint(57816, isProxied: false);
+    .WithHttpEndpoint(57816, isProxied: false)
+    .WithOtlpExporter();
 
 builder.Build().Run();
--- samples/Legacy/LegacyWebApplication/Global.asax.cs
+++ samples/Legacy/LegacyWebApplication/Global.asax.cs
@@ -1,4 +1,6 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -6,6 +8,7 @@ namespace LegacyWebApplication
 {
     public class MvcApplication : C3D.Extensions.SystemWeb.OpenTelemetry.Application.OpenTelemeteryApplication
     {
+        private IServiceProvider serviceProvider;
         protected override void Application_Start()
         {
             base.Application_Start();
@@ -14,6 +17,12 @@ namespace LegacyWebApplication
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected override void UseServiceProvider(ServiceProvider serviceProvider)
+        {
+            this.serviceProvider = serviceProvider;
+            base.UseServiceProvider(serviceProvider);
         }
     }
 }
