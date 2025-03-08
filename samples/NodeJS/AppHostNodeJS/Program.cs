c5c86ecc01e1634728be82baf86595baa8d4a292
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-03-08 21:03:09 +0000

    Improve node example with OTLP, metrics, and https.

--- samples/NodeJS/AppHostNodeJS/Program.cs
+++ samples/NodeJS/AppHostNodeJS/Program.cs
@@ -1,5 +1,7 @@
+using Aspire.Hosting;
 using AspireAppHostNodeJS;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -12,18 +14,27 @@ var prjPath = System.IO.Path.GetDirectoryName(prj.ProjectPath)!;
 var debugPort = 9229;
 
 var webapp = builder.AddNodeApp("webapp", ".", prjPath)
-    .WithHttpEndpoint(3030, isProxied: false)
+    .WithHttpEndpoint(env: "PORT")
     .WithOtlpExporter()
-    .WithDebugger(C3D.Extensions.Aspire.VisualStudioDebug.DebugMode.VisualStudio,
-        "JavaScript and TypeScript")
+    .WithDebugger(C3D.Extensions.Aspire.VisualStudioDebug.DebugMode.VisualStudio)
+    .WithDebugTransport(C3D.Extensions.Aspire.VisualStudioDebug.WellKnown.Transports.V8Inspector)
+    .WithDebugEngine(C3D.Extensions.Aspire.VisualStudioDebug.WellKnown.Engines.JavaScript)
     .WithArgs(c =>
      {
          c.Args.Insert(0, $"--inspect-wait={debugPort}");
-         //c.Args.Insert(1, "--require");
-         //c.Args.Insert(2, "./instrumentation.js");
-     });
+         c.Args.Insert(1, "--watch");
+     })
+    .WithHttpHealthCheck("alive");
 
 builder.Services.AddHostedService<NodeDebugHook>();
+
+var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"] ??
+                    builder.Configuration["AppHost:DefaultLaunchProfileName"]; // work around https://github.com/dotnet/aspire/issues/5093
+
+if (builder.Environment.IsDevelopment() && launchProfile == "https")
+{
+    webapp.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
+}
 
 
 builder.Build().Run();
--- samples/NodeJS/ExpressProject/app.js
+++ samples/NodeJS/ExpressProject/app.js
@@ -1,12 +1,97 @@
-require('./intrumentation'); // Ensure this is imported before other modules
+require('./instrumentation.js'); // Ensure this is imported before other modules
+//require('@opentelemetry/auto-instrumentations-node/register');
 
-var express = require('express');
-var app = express();
-//var otlp = require('./intrumentation');
+const http = require('node:http');
+const https = require('node:https');
+const express = require('express');
+const fs = require("node:fs");
+const { createTerminus, HealthCheckError } = require('@godaddy/terminus');
 
+const config = {
+    environment: process.env.NODE_ENV || 'development',
+    httpPort: process.env['PORT'] ?? 8080,
+    httpsPort: process.env['HTTPS_PORT'] ?? 8443,
+    httpsRedirectPort: process.env['HTTPS_REDIRECT_PORT'] ?? (process.env['HTTPS_PORT'] ?? 8443),
+    certFile: process.env['HTTPS_CERT_FILE'] ?? '',
+    certKeyFile: process.env['HTTPS_CERT_KEY_FILE'] ?? '',
+};
+console.log(`config: ${JSON.stringify(config)}`);
+
+// Setup HTTPS options
+const httpsOptions = fs.existsSync(config.certFile) && fs.existsSync(config.certKeyFile)
+    ? {
+        cert: fs.readFileSync(config.certFile),
+        key: fs.readFileSync(config.certKeyFile),
+        enabled: true
+    }
+    : { enabled: false };
+
+const app = express();
+
+function httpsRedirect(req, res, next) {
+    if (req.secure || req.headers['x-forwarded-proto'] === 'https') {
+        // Request is already HTTPS
+        return next();
+    }
+    // Redirect to HTTPS
+    const redirectTo = new URL(`https://${process.env.HOST ?? 'localhost'}:${config.httpsRedirectPort}${req.url}`);
+    console.log(`Redirecting to ${redirectTo}`);
+    res.redirect(redirectTo);
+}
+if (httpsOptions.enabled) {
+    app.use(httpsRedirect);
+}
+
+// Define health check callback
+async function healthCheck() {
+    const errors = [];
+    const apiServerHealthAddress = `${config.apiServer}/health`;
+    console.log(`Fetching ${apiServerHealthAddress}`);
+    try {
+        var response = await fetch(apiServerHealthAddress);
+        if (!response.ok) {
+            console.log(`Failed fetching ${apiServerHealthAddress}. ${response.status}`);
+            throw new HealthCheckError(`Fetching ${apiServerHealthAddress} failed with HTTP status: ${response.status}`);
+        }
+    } catch (error) {
+        console.log(`Failed fetching ${apiServerHealthAddress}. ${error}`);
+        throw new HealthCheckError(`Fetching ${apiServerHealthAddress} failed with HTTP status: ${error}`);
+    }
+}
 app.get('/', function (req, res) {
     res.send('Hello World!');
 });
-app.listen(3030, function () {
-    console.log('Example app listening on port 3030!');
-});
+// Start a server
+function startServer(server, port) {
+    if (server) {
+        const serverType = server instanceof https.Server ? 'HTTPS' : 'HTTP';
+
+        // Create the health check endpoint
+        createTerminus(server, {
+            signal: 'SIGINT',
+            healthChecks: {
+                '/health': healthCheck,
+                '/alive': () => { }
+            },
+            onSignal: async () => {
+                console.log('server is starting cleanup');
+            },
+            onShutdown: () => console.log('cleanup finished, server is shutting down')
+        });
+
+        // Start the server
+        server.listen(port, () => {
+            console.log(`${serverType} listening on ${JSON.stringify(server.address())}`);
+        });
+    }
+}
+
+const httpServer = http.createServer(app);
+const httpsServer = httpsOptions.enabled ? https.createServer(httpsOptions, app) : null;
+
+startServer(httpServer, config.httpPort);
+startServer(httpsServer, config.httpsPort);
+
+//app.listen(port, function () {
+//    console.log(`Example app listening on port ${port}!`);
+//});
