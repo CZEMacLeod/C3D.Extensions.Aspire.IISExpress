ba8aa7946305948c0b315425e704f0fba82f5c2e
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-03-08 22:19:29 +0000

    Move Node debug code to a seperate project

--- samples/NodeJS/AppHostNodeJS/DevCertHostingExtensions.cs
+++ samples/NodeJS/AppHostNodeJS/DevCertHostingExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using Aspire.Hosting.ApplicationModel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
--- samples/NodeJS/AppHostNodeJS/Program.cs
+++ samples/NodeJS/AppHostNodeJS/Program.cs
@@ -1,39 +1,22 @@
-using Aspire.Hosting;
-using AspireAppHostNodeJS;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-//var console = builder.AddProject<Projects.NodeConsoleApp>("console");
-
-var prj = new Projects.ExpressProject();
-var prjPath = System.IO.Path.GetDirectoryName(prj.ProjectPath)!;
-
-var debugPort = 9229;
-
-var webapp = builder.AddNodeApp("webapp", ".", prjPath)
+var webapp = builder.AddNodeApp<Projects.ExpressProject>("webapp")
     .WithHttpEndpoint(env: "PORT")
     .WithOtlpExporter()
-    .WithDebugger(C3D.Extensions.Aspire.VisualStudioDebug.DebugMode.VisualStudio)
-    .WithDebugTransport(C3D.Extensions.Aspire.VisualStudioDebug.WellKnown.Transports.V8Inspector)
-    .WithDebugEngine(C3D.Extensions.Aspire.VisualStudioDebug.WellKnown.Engines.JavaScript)
-    .WithArgs(c =>
-     {
-         c.Args.Insert(0, $"--inspect-wait={debugPort}");
-         c.Args.Insert(1, "--watch");
-     })
-    .WithHttpHealthCheck("alive");
-
-builder.Services.AddHostedService<NodeDebugHook>();
+    .WithWatch()
+    .WithDebugger()
+    .WithHttpHealthCheck("/alive");
 
 var launchProfile = builder.Configuration["DOTNET_LAUNCH_PROFILE"] ??
                     builder.Configuration["AppHost:DefaultLaunchProfileName"]; // work around https://github.com/dotnet/aspire/issues/5093
 
 if (builder.Environment.IsDevelopment() && launchProfile == "https")
 {
-    webapp.RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE");
+    webapp
+        .RunWithHttpsDevCertificate("HTTPS_CERT_FILE", "HTTPS_CERT_KEY_FILE")
+        .WithHttpsHealthCheck("/alive"); ;
 }
 
 
