64710108fd6047222258c6b6fbc2a8093cf846f4
Cynthia Z E MacLeod (C3D) <cynthia.macleod@cloud3d.ltd>
2025-03-14 03:45:57 +0000

    Better release notes (maybe)

---
+++ build/DetectPackages.ps1
@@ -0,0 +1,24 @@
+Import-Module .\build\ConvertTo-MarkdownTable.ps1
+$dir = $env:BUILD_ARTIFACTSTAGINGDIRECTORY + "\*.nupkg"
+Write-Host "Package Directory: $dir"
+$packages = Get-ChildItem -Path $dir -Recurse
+$ids = $packages | Select-Object -ExpandProperty name
+$pkgs = @()
+ForEach ($id in $ids) {
+	$names = $id.Split(".")
+	$name = $names[(0..($names.Length-5))] -join "."
+	$version = $names[(($names.Length-4)..($names.Length-2))] -join "."
+	$pkg = New-Object -TypeName PSObject -Property @{
+		Name = $name
+		Version = $version
+	}
+	git tag $id
+	git push origin $tag
+	$pkgs += $pkg
+}
+$pkgs | Format-Table -Property Name, Version
+Write-Host "Package Count: $($packages.Count)"
+Write-Host ("##vso[task.setvariable variable=package_count;]$($packages.Count)")
+$releaseNotes = $env:AGENT_TEMPDIRECTORY + "\ReleaseNotes.md"
+"## Packages" | Out-File $releaseNotes
+$pkgs | ConvertTo-MarkdownTable | Add-Content $releaseNotes
